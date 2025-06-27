5d1fcc2fd88da0451e26a14097bce76e75e7124f
oshockley <onealshockley@gmail.com>
2025-06-27 11:43:55 -0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+        {/* Theme Toggle */}
+
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+    </div>;
+};
